423bbae8d38f2bab7bf964bd64f322d4766e8afd
sachindu01 <e20305@eng.pdn.ac.lk>
2025-10-18 14:27:56 +0530

    Comments to the user model new

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Entity
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 @Table(name = "users") //this is just the database view for the user
-//Sample comment just to trigger tests
+//Sample comment just to trigger tests abcd
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
